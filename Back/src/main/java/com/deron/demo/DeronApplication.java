fbe38e00b799c68bec8727d255476560cc2e4cf5
BoujemaaWahid <boujemaa.wahid@gmail.com>
2020-08-09 01:51:04 +0200

    Init sprint boot back app

---
+++ Back/src/main/java/com/deron/demo/DeronApplication.java
@@ -0,0 +1,13 @@
+package com.deron.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeronApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/deron/demo/DeronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deron.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
