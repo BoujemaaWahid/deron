20b68ef917c6dcf835b1ef5e5a0eec218e32e391
BoujemaaWahid <boujemaa.wahid@gmail.com>
2020-08-17 16:15:51 +0200

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 declare var $: any;
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,7 +8,23 @@ declare var $: any;
 })
 export class ProfileComponent implements OnInit {
   t = []
+  messages = [
+    { width:'', msg: "hello" },
+    { width:'', msg: "hello 123 456 789" },
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132"},
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132 789 456 123 789 456 123 789 456 132"},
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132 789 456 123 789 456 123 789 456 132AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"},
+    { width:'', msg: "hello" },
+    { width:'', msg: "hello 123 456 789" },
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132"},
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132 789 456 123 789 456 123 789 456 132"},
+    { width:'', msg: "hello 789 456 123 789 456 123 789 456 132 789 456 123 789 456 123 789 456 132AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"}
+  
+  ]
   constructor() { 
+    for(let j = 0; j< this.messages.length; j++){
+      this.messages[j].width= (this.messages[j].msg.length + 5) + '%' 
+    }    
     for (let i = 0; i < 100; i++){ this.t.push(i) }
   }
 
